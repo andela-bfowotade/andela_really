e408ce4ace3aa386ac764fb181a6ff54a8605a3a
Jihdeh Stephens <jide@Jihdehs.local>
2015-03-05 22:44:17 +0100

    port env config added

--- areallybigpage.js
+++ areallybigpage.js
@@ -4,7 +4,7 @@ var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var fs = require('fs');
 
-server.listen(5000);
+server.listen(process.env.PORT || 5000);
 
 app.use(express.static(__dirname + '/public'));
 
